fef5b13f0c7537ea0928c635b1e899d87e338493
郭清 <guoqing2013@163.com>
2013-10-12 00:12:03 +0800

    YinLiZi-fore

---
+++ js/serverurl.js
@@ -0,0 +1,10 @@
+//var SERVER = 'http://192.168.1.111/ylz/code/index.php'; //钮
+//var SERVER_ROOT = 'http://192.168.1.111/ylz/code/';
+
+//var SERVER = 'http://192.168.1.123/ylz/code/index.php'; //朱
+//var SERVER_ROOT = 'http://192.168.1.123/ylz/code/';
+
+var SERVER = 'http://yunku.4pu.com/yzd/code/index.php'; //服务器
+var SERVER_ROOT = 'http://yunku.4pu.com/yzd/code/';
+var IMG_SERVER_ROOT = 'http://yunku.4pu.com/yzd/code/';/*图片文件另外指定*/
+
